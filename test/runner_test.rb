8722be721213917679e11344b2d6d49637ef1489
adamliesko <adamliesko@gmail.com>
2016-03-21 11:02:24 +0100

    Use relative path in a RunnerTest for data input folder

--- test/runner_test.rb
+++ test/runner_test.rb
@@ -6,7 +6,7 @@ require File.expand_path('../../lib/runner', __FILE__)
 class RunnerTest < Minitest::Test
   def test_runnner_run_initializes_and_runs_the_processing
     anonymize_function = -> (_purchase_kind, count) { count > 5 }
-    stats = Runner.run('/Users/Adam/aircloak_challenge/data', 'airline', &anonymize_function)
+    stats = Runner.run(File.expand_path('../../data', __FILE__), 'airline', &anonymize_function)
     assert_equal 150, stats[:min]
     assert_equal 150, stats[:max]
     assert_equal 150, stats[:avg]
